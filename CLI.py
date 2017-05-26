6d0c2e9ed8a845b24f580fe944c0f92194a86f4c
Richard Young <richard_young@umail.ucsb.edu>
2017-05-26 15:48:08 -0700

    CLI fully written, but untested -- need other parts to test

--- CLI.py
+++ CLI.py
@@ -51,14 +51,73 @@ class cli:
                 print("setup complete")
 
                 #move onto command execution
-                self.execute_commands()
+                self.commands()
 
 
 	def commands(self):
 		#we need to implement the replicate, stop, resume, total, print merge 
-
-	def wait(self):
+                while True:
+                    input_string = input("Input command: ")
+                    input_string = input_string.strip().split()
+                    
+                    #set up blank message
+                    msg = ''
+                    #character "*" used as delimiter for future message processing
+                    if input_string[0] == 'print':
+                        msg = 'print*'
+                    elif input_string[0] == 'stop':
+                        msg = 'stop*'
+                    elif input_string[0] == 'resume':
+                        msg = 'resume*'
+                    #for messages with multiple args, use "|" as splitter
+                    elif input_string[0] == 'merge':
+                        #if correct num of args
+                        if len(input_string) == 3:
+                            msg = 'merge|'+input_string[1]+'|'+input_string[2]+'*'
+                        else:
+                            print("Invalid number of args, need 3 args.")
+                    elif input_string[0] == 'total':
+                        #if correct num of args
+                        if len(input_string) == 3:
+                            msg = 'total|'+input_string[1]+'|'+input_string[2]+'*'
+                        else:
+                            print("Invalid number of args, need 3 args.")
+                    if input_string[0] == 'replicate':
+                        #if correct num of args
+                        if len(input_string) == 2:
+                            f = input_string[1] #filename
+                            msg = 'replicate|'+f+'*'
+                        else:
+                            print("Invalid number of args, need 2 args.")
+                    else:
+                        print("no matching command found.")
+                        print("Valid commands: print, stop, resume, merge, total, replicate")
+                    
+                    #send msg to all sockets
+                    self.socket_prm_out.sendall(msg.encode())
+                    #if msg has something, then wait.
+                    if msg != '':
+                        self.wait_response()
+                    
+	def wait_response(self):
 		#we need to waitfor a single process to finish before we do another
+		while True:
+		    try:
+		        data_recv = self.socket_prm_in.recv(1024).decode()
+		        print("Data received: ", data_recv)
+
+		        data_proc = data_recv.strip().split('*')
+		        if len(data_recv) >= 1:
+		            if data_proc[0] == 'ack':
+		               print('msg successfully passed and exed')
+		               return
+		            if data_proc[0] == 'nack':
+		                print('msg NOT successfully passed and exed--investigate further!')
+		                return
+		    except socket.error:
+		        print("Socket error experienced--investigate further :(")
+		        continue
+		    time.sleep(1)
 
 
 		#TODO:
