aa161453f55e94ae0e4dcf0074ffbbc614939e6d
James Yang <jjyang@umail.ucsb.edu>
2017-05-26 17:02:34 -0700

    paxos in prog

--- paxos.py
+++ paxos.py
@@ -1,11 +1,82 @@
 import sys
 import socket 
 import time
+import select
 
-class Paxos():
+incomingTCP = dict()
+outgoingTCP = dict()
+sites = []
 
-	def _init_(self, index, id):
-		#constructor 
+
+stop = False #if a process gets stopped it'll be true
+
+class Paxos(object):
+
+	def _init_(self, index, id ):
+		self.accepts = dict()
+		self.ballotNum = [0,0]
+		self.ackList = [] #we need to keep track of the acks from other sites
+		self.propVal = None #null until a value has been proposed
+		self.acceptNum = [0,0]
+		self.numVotes = 1
+		self.leader = False #starts at false 
+		self.acceptVal = None #null until a value has been accepted 
+		self.id = id
+
+	def reinit():
+		self.accepts.clear()
+		self.ballotNum = [0,0]
+		self.ackList.clear() #we need to keep track of the acks from other sites
+		self.propVal = None #null until a value has been proposed
+		self.acceptNum = [0,0]
+		self.numVotes = 1
+		self.leader = False #starts at false 
+		self.acceptVal = None #null until a value has been accepted 
+		
+
+
+	def total(files):
+		total = 0
+		for filename in files:
+			with open(filename) as f:
+				for line in f.readlines():
+					with open(filename) as f:
+						for line in f.readlines():
+							currentLine = line.split()
+							counter = int(currentLine[1])
+							total += counter
+		return total
+
+
+
+	def printFiles(object):
+		for data in the object:
+			print(data[0])
+
+	def merge(files)
+	#merge cmd implemented
+		words = dict()
+		for filename in files:
+			with open(filename) as f:
+				for line in f.readlines():
+					currentLine = line.split()
+					word = currentline[0]
+					count = int(currentLine[1])
+					if(word in words.keys()):
+						oldCount = words.get(word)
+						newCount = int(oldCount + count)
+						words[word] = newCount
+					else:
+						words[word] = count
+
+		print(words)
+		return words
+
+
+
+
+	
+	
 
 	def rcvPrep(self, data, channel):
 
