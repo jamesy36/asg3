576239c97a344afdbe36b24151b8bcc9cd7dacc5
James Yang <jjyang@umail.ucsb.edu>
2017-05-27 17:16:12 -0700

    finished paxos implementation, need to test with CLI

--- PRM.py
+++ PRM.py
@@ -5,7 +5,7 @@ import select
 
 incomingTCP = dict()
 outgoingTCP = dict()
-sites = []
+siteInfo = []
 
 log = [] #holds all of the instances for paxos during the process
 
@@ -20,7 +20,7 @@ class PRM(object):
 	def _init_(self, index, id ):
 		self.accepts = dict()
 		self.ballotNum = [0,0]
-		self.ackList = [] #we need to keep track of the acks from other sites
+		self.ackList = [] #we need to keep track of the acks from other siteInfo
 		self.propVal = None #null until a value has been proposed
 		self.acceptNum = [0,0]
 		self.numVotes = 1
@@ -31,7 +31,7 @@ class PRM(object):
 	def reinit():
 		self.accepts.clear()
 		self.ballotNum = [0,0]
-		self.ackList.clear() #we need to keep track of the acks from other sites
+		self.ackList.clear() #we need to keep track of the acks from other siteInfo
 		self.propVal = None #null until a value has been proposed
 		self.acceptNum = [0,0]
 		self.numVotes = 1
@@ -55,7 +55,7 @@ class PRM(object):
 
 
 	def printFiles(log):
-		for data in the log:
+		for data in log:
 			print(data[0])
 
 	def merge(files)
@@ -82,20 +82,20 @@ class PRM(object):
 	def acknowledgeCheck(logs):
 		for ack in logs:
 			if(ack[2][0] != None and ack[2][1] != None ):
-				return False
+				return True
 
-		return True  
+		return False 
 
 
 	def highestBallot(logs):
 		ballotNum = 0
-		site = 0
+		siteNum = 0
 		highest = [0,0]
 		for ack in logs:
-			if(logs[0][0] > ballotNum or (logs[0][0] == balllotNum and log[0][1] > site)):
+			if(logs[0][0] > ballotNum or (logs[0][0] == ballotNum and log[0][1] > siteNum)):
 				ballotNum = logs[0][1]
-				site = logs[0][1]
-				highest = [ballotNum, site]
+				siteNum = logs[0][1]
+				highest = [ballotNum, siteNum]
 
 		return highest
 
@@ -109,58 +109,172 @@ class PRM(object):
 			if(ready[0]):
 				data = sock.recv(1024).decode()
 				splitData = data.split("*")
-				dataList = splitData[0].split()
-				print(dataList)
+				process = splitData[0].split()
+				print(process)
 
-				if(not dataList or stop):
+				if(not process or stop):
 					print("process has been stopped/the selected node is empty")
 					continue
-				elif(dataList[0].find("total") != -1):
+				elif(process[0].find("total") != -1):
 					print("total")
-				elif(dataList[0].find("print") != -1):
+				elif(process[0].find("print") != -1):
 					print("print")
 					printFiles(log) 
-				elif(dataList[0].find("merge") != -1):
+				elif(process[0].find("merge") != -1):
 					print("merge")
-					f1 = log[int(dataList[1])][0]
-					f2 = log[int(dataList[2])][0]
+					f1 = log[int(process[1])][0]
+					f2 = log[int(process[2])][0]
 					files = [f1, f2]
 					merge(files)
-				elif(dataList[0].find("replicate") != -1):
+				elif(process[0].find("replicate") != -1):
 					print("replicate")
 					prm.ballotNum[0] += 1
-					prm.ballotNum[1] = site
-					prm.propVal[0] = dataList[1]
+					prm.ballotNum[1] = siteNum
+					prm.propVal[0] = process[1]
 					prm.leader = True
 					prepSend = "prepare" + str(prm.ballotNum[0]) + " " + str(prm.balltNum[1]) + "*"
 					for c in outgoingTCP.keys():
 						if(c != "cli"):
 							sock = outgoingTCP.get(c)
-							sock.sendall(toSend.encode())
-				elif(dataList[0].find("stop") != -1):
+							sock.sendall(prepSend.encode())
+				elif(process[0].find("stop") != -1):
 					stop = True
-				elif(dataList[0].find("resume") != -1):
+				elif(process[0].find("resume") != -1):
 					print("resume")
 					stop = False
+				elif(process[0].find("prepare") != -1):
+					print("prepare")
+					ballot = int(dataList[1])
+					siteNum = dataList[2] 
+					#id for the siteNum
+					if(prm.ballotNum[0] < ballot or (prm.ballotNum[0] == ballot and prm.ballotNum[1] < int(siteNum))):
+						prm.ballotNum[0] = ballot 
+						prm.ballotNum[1] = siteNum
+						prepSend = "ack" + str(prm.ballotNum[0]) + " " + str(prm.ballotNum[1])
+						sock = outgoingTCP.get(siteNum)
+						sock.sendall(prepSend.encode())
+				elif(process[0].find("ack") != -1):
+					print("acknowledge")
+					balNum = [process[1], process[2]]
+					acceptBal = [process[3], process[4]]
+					acceptVal = [process[5], process[6]]
+					rcvsiteNum = process[7]
+					thisAck = [balNum, acceptBal, acceptVal, rcvsiteNum]
+					prm.acklist.append(thisAck)
+					if(len(prm.ackList) >= 2):
+						#if majority of acks
+						if(acknowledgeCheck(prm.ackList)):
+							#if yes and there is a value
+							prm.acceptVal = highestBallot(prm.ackList)[:]
+						else:
+							prm.acceptVal = prm.propVal[:]
+						for c in outgoingTCP.keys():
+							if( c != "cli"):
+								sock = outgoingTCP.get(c)
+								prepSend = "accept" + str(prm.ballotNum[0]) + " " + str(prm.ballotNum[1]) + " " + prm.acceptVal[0] + " " + prm.acceptVal[1] + "*"
+								sock.sendall(prepSend.encode())
+				elif(process.find("accept") != -1):
+					bal = str(process[1]) + str(process[2])
+					if(bal in prm.acceptNum):
+						prm.acceptNum[bal] += 1
+					else:
+						prm.acceptNum[bal] = 1
+					if(int(prm.ballotNum[0]) < int(process[1]) or (int(prm.ballotNum[0]) == int(process[1]) and int(prm.ballotNum[1]) < int(process[2]))):
+						prm.ballotNum[0] = process[1]
+						prm.ballotNum[1] = process[2]
+						prm.acceptBal[0] = process[1]
+						prm.acceptBal[1] = process[2]
+						acceptVal = [process[3], process[4]]
+						prm.acceptVal = accVal[:]
+					if(prm.acceptNum[bal] == 1 ):
+						prepSend = "accept" + str(process[1]) + " " + str(process[2]) + " " + str(process[3]) + " " + str(process[4]) + "*"
+						for c in outgoingTCP.keys():
+							if(c != "cli"):
+								sock = outgoingTCP.get(c)
+								sock.snedall(prepSend.encode())
+					if(prm.acceptNum[bal] >= 2 and prm.leader):
+						prm.append([prm.acceptVal[0], prm.acceptVal[1]])
+						prepSend = "decide" + str(prm.acceptVal[0]) + " " + str(prm.acceptVal[1]) + "*"
+						for c in outgoinTCP.keys():
+							sock = outgoingTCP.get(c)
+							sock.sendall(prepSend.encode())
+						prm.reinit
+				elif(process[0].find("decide") != -1):
+					acceptVal = [process[1], process[2]]
+					prm.append(acceptVal)
+					prm.reinit
+					sys.stdout.write("Printing prm: ")
+					print(prm)
+				else
+					continue
+		return
 
 
 
-#TODO: implement ack, rest of paxos implementation
+#Create the server
+server = socket(AF_INET, SOCK_STREAM)
+server.setsockopt(SOL_SOCKET, SO_RESUSEADDR, 1)
+
+siteNum = sys.argv[1]
+setup = sys.argv[2]
+print("Esablishing connection!")
+with open(setup) as f:
+	numSites = f.readline().strip() 
+	for i in range(int(numSites)):
+		line = f.readline().stirp().split()
+		siteInfo.append([data for data in line])
+
+	server.bind(('', int(siteInfo[int(siteNum) -1][1])))
+	server.listen(10)
+
+	for line in f.readlines():
+		nums = line.strip().split()
+		if(nums[0] == siteNum):
+			ip = siteInfo[int(nums[1]) -1][0]
+			port = siteInfo[int(nums[1])-1][1]
+			s = socket(AF_INET, SOCK_STREAM)
+			addr = (ip, int(port))
+			time.sleep(10)
+			try:
+				s.connect(addr)
+				print("Connected to ", nums[1])
+			except error:
+				time.sleep(5)
+			outgoingTCP[nums[1]] = s
+		if(nums[1] == siteNum):
+			connect = server.accept()
+			addr = server.accept()
+			incomingTCP[nums[0]] = connect
+
+#Receiving connection from the CLI
+
+connect = server.accept()
+addr = server.accept()
+incomingTCP["cli"] = connect
+print(connect)
+princt(addr)
+
+#Opening a connection with the CLI
+
+cliPort = int(siteInfo[int(siteInfo) - 1][1]) + 3 
+s = socket(AF_INET, SOCK_STREAM)
+addr = ("127.0.0.1", cliPort)
+time.sleep(5)
+s.connect(addr)
+outoingTCP["cli"] = s
+
+print("Ready for commands")
+while(True):
+	receive(incomingTCP)
 
 
 
 
-	def rcvPrep(self, data, channel):
-
-
-
-	def rcvAck(self, data, channel):
 
 
 
 
 
-	def prepare(self, incomingMessages):
 
 
-	def resolution()
+
