8a8c5bdc94734107f1a8e795f34baa27b43840da
James Yang <jjyang@umail.ucsb.edu>
2017-05-27 21:53:32 -0700

    configged to each other, need to test

--- CLI.py
+++ CLI.py
@@ -1,7 +1,24 @@
 import socket
 import time
+import sys
 
 class cli:
+   
+    ip = "127.0.0.1"
+    nodePort = sys.argv[1]
+    prmPort = sys.argv[2]
+    #mapPort1 = sys.argv[3]
+    #mapPort2 = sys.argv[4]
+    #reducerPort = sys.argv[5]
+
+    incomingTCP = []
+    outgoingTCP = []
+    sock = socket(AF_INET, SOCK_STREAM)
+
+
+
+
+
 	def _init_(self, id):
 		#needs to be able to determine how many sockets there will be
 		#needs to keep track of prm sockets
@@ -14,66 +31,62 @@ class cli:
 
 	def setup(self):
 		#setup the cli
-		address = ('127.0.0.1', 6000 + self.id)
-		address_out = ('127.0.0.1', 5000 +self.id)
+		address = (ip, int(prmPort))
+        time.sleep(5)
 
 		print("Init' the CLI at the address: ", address)
                 
-                #set up sockets
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		sock.bind(address)
-		s.listen(1)
-		connection, address_in = sock.accept()
-                connection.setblocking(0)
-                self.socket_prm_in = connection
+         #set up sockets 
+		sock.connect(address)
+        outgoingTCP.append(sock)
+        print("CONNECTED")
 
-                #when connection is successful
-                print("conneection accepted from the PRM")
-
-                #now try to connect to the PRM
-                out_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-               
-                #keep trying until it works!!
-                while True:
-                    try:
-                        out_sock.connect(address_out)
-                        out_sock.setblocking(0)
-                        #will only get here if previous 2 steps worked
-                        print("connection established at address: ", address_out)
-
-                        break
-                    except socket.error:
-                        #if it didn't work, just sleep and try again
-                        time.sleep(1)
-
-                self.socket_prm_out = out_sock
-                #setup complete, notify CLI
-                print("setup complete")
-
-                #move onto command execution
-                self.commands()
+        #Start server for PRM
+        server = socket(AF_INET, SOCK_STREAM)
+        server.setsockopt(SOL_SOCKET, SO_RESUSEADDR, 1)
+        server.bind(('', int(nodePort)))
+        server.listen(10)
+        print("Receiving connection from RPMS")
+        connection = server.accept()
+        address = server.accept()
+        incomingTCP.append(connection)
+        print("Connection Successful")
+		 #move onto command execution
+         self.commands()
 
 
 	def commands(self):
 		#we need to implement the replicate, stop, resume, total, print merge 
-                while True:
-                    input_string = input("Input command: ")
-                    input_string = input_string.strip().split()
+                for line in sys.stdin:
+                    print(line)
+                    input_string = line.split()
                     
                     #set up blank message
-                    msg = ''
+                    # msg = ''
                     #character "*" used as delimiter for future message processing
                     if input_string[0] == 'print':
-                        msg = 'print*'
+                        if(len(input_string) != 1):
+                            print("Print")
+                            continue
+                        sock.sendall("Print*".encode())
                     elif input_string[0] == 'stop':
-                        msg = 'stop*'
+                        if(len(input_string) != 1):
+                            print("stop")
+                            continue
+                        sock.sendall("stop*".encode())
                     elif input_string[0] == 'resume':
-                        msg = 'resume*'
+                        if(len(input_string) != 1):
+                            print("resume")
+                            continue
+                        sock.sendall("resume*".encode())
                     #for messages with multiple args, use "|" as splitter
                     elif input_string[0] == 'merge':
                         #if correct num of args
                         if len(input_string) == 3:
-                            msg = 'merge|'+input_string[1]+'|'+input_string[2]+'*'
+                            f1 = input_string[1]
+                            f2 = input_string[2]
+                            data = "merge" + f1 + " " + f2 + "*"
+                            sock.sendall(data.encode())
                         else:
                             print("Invalid number of args, need 3 args.")
                     elif input_string[0] == 'total':
@@ -82,21 +95,20 @@ class cli:
                             msg = 'total|'+input_string[1]+'|'+input_string[2]+'*'
                         else:
                             print("Invalid number of args, need 3 args.")
+                            continue
                     if input_string[0] == 'replicate':
                         #if correct num of args
                         if len(input_string) == 2:
                             f = input_string[1] #filename
-                            msg = 'replicate|'+f+'*'
+                            data = "replicate" + input_string[1] + "*"
+                            sock.sendall(data.encode())
                         else:
                             print("Invalid number of args, need 2 args.")
                     else:
                         print("no matching command found.")
                         print("Valid commands: print, stop, resume, merge, total, replicate")
-                    
-                    #send msg to all sockets
-                    self.socket_prm_out.sendall(msg.encode())
-                    #if msg has something, then wait.
-                    if msg != '':
+                                       
+                    if input_string != '':
                         self.wait_response()
                     
 	def wait_response(self):
@@ -120,6 +132,4 @@ class cli:
 		    time.sleep(1)
 
 
-		#TODO:
-		#So basically we can either implement the functions in here, or in the PRM 
-		#It seems that we can just send messages to tell PRM to specific stuff
+		
