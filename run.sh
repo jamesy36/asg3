07c4e8ba825e589cb7d079a948484f447f4bf916
ryoung141 <ryoung141@gmail.com>
2017-06-10 23:54:03 +0000

    added arg comparison for script

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo "Running, usage: ./run.sh node_number setup*.txt"
 
-if [[ $# -eq 0]] ; then
-    echo "Illegal number of parameters"
-    exit 1
-fi
+if [ $# -eq 0 ]
+  then
+          echo "No arguments supplied"
+          exit 1
+      fi
 
 python PRM.py $1 $2&
 python mapper.py 5002 5001 1&
