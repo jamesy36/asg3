22add32261bde48e05006637310bc93c178fe979
ryoung141 <ryoung141@gmail.com>
2017-06-10 23:48:32 +0000

    updated script

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 echo "Running, usage: ./run.sh node_number setup*.txt"
+
+if [[ $# -eq 0]] ; then
+    echo "Illegal number of parameters"
+    exit 1
+fi
+
 python PRM.py $1 $2&
 python mapper.py 5002 5001 1&
 python mapper.py 5003 5001 2&
